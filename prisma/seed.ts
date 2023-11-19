07dc9a23088edd6925a34922b4e660a9eb1feb15
fltmzl <fltmzl2810@gmail.com>
2023-11-19 09:58:31 +0700

    feat(products): add database category table seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -13,7 +13,17 @@ async function main() {
     },
   });
 
-  console.log({ ahmad });
+  const electronic = await prisma.category.upsert({
+    where: { id: 'c58d789f-c681-4b36-9710-411bfbb6f7b3' },
+    update: {},
+    create: {
+      id: 'c58d789f-c681-4b36-9710-411bfbb6f7b3',
+      name: 'electronic',
+      description: 'desc elektronik',
+    },
+  });
+
+  console.log({ ahmad, electronic });
 }
 
 main()
