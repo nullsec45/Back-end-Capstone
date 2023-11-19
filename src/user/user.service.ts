a8116ac99475d55f7363641a79d0ba448c6adbc0
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-11-20 02:18:49 +0700

    feat(auth):user register

---
+++ src/user/user.service.ts
@@ -0,0 +1,76 @@
+import { Injectable, ConflictException, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma.service';
+import { hash } from "bcryptjs";
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) { }
+
+
+  async create(createUserDto: CreateUserDto) {
+    const checkEmail = await this.findByEmail(createUserDto.email);
+
+    if (checkEmail) {
+      throw new ConflictException("email duplicated");
+    }
+
+    const newUser = await this.prisma.user.create({
+      data: {
+        ...createUserDto,
+
+        password: await hash(createUserDto.password, 10)
+      }
+    });
+    const { password, ...data } = newUser;
+
+    return {
+      data,
+      statusCode: 201,
+      message: "User Successfully registerd",
+    };
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  async findOne(id: string) {
+    console.log(id);
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id
+      }
+    })
+
+    const { password, ...data } = user;
+
+    if (user) {
+      return {
+        data,
+        statusCode: 200
+      }
+    }
+
+    throw new NotFoundException("Data user tidak ditemukan")
+  }
+
+  async findByEmail(email: string) {
+    const emailUser = await this.prisma.user.findUnique({
+      where: {
+        email,
+      }
+    })
+
+    return emailUser;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
