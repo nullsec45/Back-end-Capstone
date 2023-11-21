44b2cb7956e8f07102bf4f202590c85fba54ca82
fltmzl <fltmzl2810@gmail.com>
2023-11-21 07:46:01 +0700

    feat(orders): add rentPeriod property to the DTO

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,56 @@
-export class CreateOrderDto {}
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  ArrayNotEmpty,
+  ValidateNested,
+} from 'class-validator';
+
+class ProductDto {
+  @IsNotEmpty()
+  @IsString()
+  id: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  quantity: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  rentPeriod: number;
+}
+
+class TransactionDto {
+  @IsNotEmpty()
+  @IsEnum(['TRANSFER'])
+  paymentMethod: string;
+}
+
+enum ShippingMethod {
+  PICKUP = 'PICKUP',
+  GOSEND = 'GOSEND',
+}
+
+export class CreateOrderDto {
+  @IsNotEmpty()
+  @IsString()
+  userAddressId: string;
+
+  @IsNotEmpty()
+  @IsEnum(ShippingMethod)
+  shipping: ShippingMethod;
+
+  @ArrayNotEmpty()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ProductDto)
+  products: ProductDto[];
+
+  @IsNotEmpty()
+  @ValidateNested()
+  @Type(() => TransactionDto)
+  transaction: TransactionDto;
+}
