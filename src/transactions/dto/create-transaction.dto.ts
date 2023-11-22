14c6b2957ffd22bb943941260a370305501ee811
fltmzl <fltmzl2810@gmail.com>
2023-11-22 10:46:18 +0700

    feat(transactions): add DTO for request body transaction

--- src/transactions/dto/create-transaction.dto.ts
+++ src/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,11 @@
-export class CreateTransactionDto {}
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateTransactionDto {
+  @IsNotEmpty()
+  @IsString()
+  paymentProof: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  transactionAmount: number;
+}
