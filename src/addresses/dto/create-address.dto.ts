3e9efda735ec63a4020c1076740bd05da976f2be
fltmzl <fltmzl2810@gmail.com>
2023-11-22 22:48:05 +0700

    feat(addresses): add DTO for request body

--- src/addresses/dto/create-address.dto.ts
+++ src/addresses/dto/create-address.dto.ts
@@ -1,1 +1,31 @@
-export class CreateAddressDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateAddressDto {
+  @IsNotEmpty()
+  @IsString()
+  province: string;
+
+  @IsNotEmpty()
+  @IsString()
+  city: string;
+
+  @IsNotEmpty()
+  @IsString()
+  district: string;
+
+  @IsNotEmpty()
+  @IsString()
+  fullAddress: string;
+
+  @IsNotEmpty()
+  @IsString()
+  postalCode: string;
+
+  @IsNotEmpty()
+  @IsString()
+  latitude: string;
+
+  @IsNotEmpty()
+  @IsString()
+  longitude: string;
+}
