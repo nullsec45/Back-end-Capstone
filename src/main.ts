4199f7d2ce37e69eb5b3ada6ec28f603be93f14b
fltmzl <fltmzl2810@gmail.com>
2023-12-02 19:11:12 +0700

    fix(app): change PORT configuration main.ts

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
