987be133118c6b4d816184a455305049a6a6f193
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-12-08 17:21:28 +0700

    feat(profile):new profile module

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
+import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [ProfileController],
-  providers: [ProfileService],
+  providers: [ProfileService, PrismaService],
 })
-export class ProfileModule {}
+export class ProfileModule { }
