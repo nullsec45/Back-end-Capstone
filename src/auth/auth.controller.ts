f8894cb7b3f223fa59f43937ef744a19de085ca0
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-12-09 17:52:40 +0700

    documentation:open api documentation for endpoint auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,19 +9,42 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../users/dto/create-user.dto';
+import { AuthDto } from './dto/auth.dto';
 import { UsersService } from '../users/users.service';
 import { LocalAuthGuard } from './guard/local-auth.guard';
 import { JwtAuthGuard } from './guard/jwt-auth.guard';
+import {
+  ApiTags,
+  ApiBearerAuth,
+  ApiBody,
+  ApiResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(
     private readonly authService: AuthService,
     private userService: UsersService,
-  ) {}
+  ) { }
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
+  @ApiBody({
+    type: AuthDto,
+    description: 'request body for auth login',
+  })
+
+  @ApiResponse({
+    status: 201,
+    description: 'login sucessfully',
+  })
+
+  @ApiResponse({
+    status: 401,
+    description: 'invalid credentials'
+  })
   async login(@Req() req: any) {
     const accessToken = await this.authService.login(req.user);
 
@@ -34,6 +57,9 @@ export class AuthController {
     };
   }
 
+  @ApiBearerAuth()
+  @ApiUnauthorizedResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 200, description: 'token valid' })
   @UseGuards(JwtAuthGuard)
   @Get('verify-token')
   async verifyToken(@Req() req: any) {
@@ -44,6 +70,25 @@ export class AuthController {
     };
   }
 
+  @ApiBody({
+    type: CreateUserDto,
+    description: 'request body for auth register',
+  })
+
+  @ApiResponse({
+    status: 201,
+    description: 'user sucessfully registerd',
+  })
+
+  @ApiResponse({
+    status: 409,
+    description: 'email duplicated',
+  })
+
+  @ApiResponse({
+    status: 409,
+    description: 'username duplicated',
+  })
   @Post('register')
   async register(@Body() createUserDto: CreateUserDto) {
     const newUser = await this.userService.create(createUserDto);
