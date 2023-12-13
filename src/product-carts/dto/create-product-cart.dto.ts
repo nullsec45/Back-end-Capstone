3fec54bb35605195ca991435d8f7fa0aa3de784e
fltmzl <fltmzl2810@gmail.com>
2023-12-13 13:03:42 +0700

    fix(cart): add rentFrom and rentTo column

--- src/product-carts/dto/create-product-cart.dto.ts
+++ src/product-carts/dto/create-product-cart.dto.ts
@@ -1,18 +1,22 @@
-import {
-    IsNotEmpty,
-    IsNumber,
-    IsString,
-} from "class-validator";
+import { IsDateString, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CreateProductCartDto {
-    userId: string
-    cartId: string
+  userId: string;
+  cartId: string;
 
-    @IsNotEmpty()
-    @IsString()
-    productId: string
+  @IsNotEmpty()
+  @IsString()
+  productId: string;
 
-    @IsNotEmpty()
-    @IsNumber()
-    quantity: number
+  @IsNotEmpty()
+  @IsNumber()
+  quantity: number;
+
+  @IsNotEmpty()
+  @IsDateString()
+  rentFrom: string;
+
+  @IsNotEmpty()
+  @IsDateString()
+  rentTo: string;
 }
