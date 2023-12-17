13214f663c9aa3ac7cbaa13fe327ee20142779bb
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-12-17 15:17:30 +0700

    documentation:open api swagger transactions

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -12,7 +12,12 @@ import { TransactionsService } from './transactions.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 import { AuthenticatedRequest } from '../../typings';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiUnauthorizedResponse,
+  ApiResponse,
+  ApiBody
+} from '@nestjs/swagger';
 
 @ApiTags('transactions')
 @UseGuards(JwtAuthGuard)
@@ -20,7 +25,19 @@ import { ApiTags } from '@nestjs/swagger';
 export class TransactionsController {
   constructor(private readonly transactionsService: TransactionsService) { }
 
+  @ApiBody({
+    description: 'request body post transaction',
+    type: CreateTransactionDto
+  })
   @Post(':id/payment-proof')
+  @ApiResponse({
+    status: 200,
+    description: 'payment proof successfully sent. please wait for confirmation from the store owner',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'you are not allowed to send this payment proof',
+  })
   @HttpCode(HttpStatus.OK)
   async sendPaymentProof(
     @Param('id') transactionId: string,
@@ -43,6 +60,14 @@ export class TransactionsController {
   }
 
   @Post(':id/approve-payment')
+  @ApiResponse({
+    status: 200,
+    description: 'payment is approved. please process the product to be shipped',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'you are not allowed to approve this payment proof. you are not the owner of this store',
+  })
   @HttpCode(HttpStatus.OK)
   async approvePayment(
     @Param('id') transactionId: string,
@@ -63,6 +88,14 @@ export class TransactionsController {
   }
 
   @Post(':id/reject-payment')
+  @ApiResponse({
+    status: 200,
+    description: 'payment is rejected. please give the user a reason why the payment was rejected',
+  })
+  @ApiResponse({
+    status: 403,
+    description: 'you are not allowed to reject this payment proof. you are not the owner of this store',
+  })
   @HttpCode(HttpStatus.OK)
   async rejectPayment(
     @Param('id') transactionId: string,
